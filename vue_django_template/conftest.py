8e8e4c57d9d3d9562a41df439eaa71a7bb60e6c2
rosstripi <rosstripi@gmail.com>
2023-02-18 23:31:55 -0500

    Vue and Django files already dockerized

---
+++ vue_django_template/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from vue_django_template.users.models import User
+from vue_django_template.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
